35c7a2c160f1ca29102f1125ad693a0abc6aba41
Sattar Amrahov <settar216@gmail.com>
2019-06-04 18:35:14 +0400

    files added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # P210-20190604-ASP.NET-Template
+
+Faydalı Linklər:
+
+- http://www.httrack.com/page/2/
+- https://themeforest.net/item/reckondit-audit-multipage-html5-template/22922777
+- https://www.completecsharptutorial.com/asp-net-mvc5/asp-net-mvc-5-renderbody-renderpage-and-rendersection-with-example.php
+- https://www.tutorialsteacher.com/mvc/partial-view-in-asp.net-mvc
---
+++ Template/Template/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Template.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Single()
+        {
+            return View();
+        }
+    }
+}
